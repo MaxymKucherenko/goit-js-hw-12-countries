cd98732831e7bfe124a388655202daf63a7ed3f6
MaxymKucherenko <maxym_88@ukr.net>
2021-06-04 02:29:57 +0300

    final build

--- src/js/SawapAllerts.js
+++
@@ -1,8 +0,0 @@
-import Swal from 'sweetalert2';
-
-export const swalAlert = message => {
-  Swal.fire({
-    icon: 'error',
-    text: `Something went wrong! -  ${message}`,
-  });
-};
--- src/js/fetchCountries.js
+++
@@ -1,8 +0,0 @@
-export const fetchCountries = async name => {
-  let response = await fetch(`https://restcountries.eu/rest/v2/name/${name}`);
-  const data = await response.json();
-  if (!response.ok) {
-    throw new Error(data.message || 'Something wrong');
-  }
-  return data;
-}; 
